fc39892df6282075d28e53ade05d6256b0270f2b
zoechi <gzoechi@gmail.com>
2014-09-16 14:42:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+#BWU Emoji One iconset SVG
+
+##Emoji provided free by [Emoji One](http://emojione.com)
+
+###Emoji One emoji graphics are free to use for any project, commercial or personal under a "Free Culture" Creative Commons License (CC-BY-SA). Attribution is required for the rights to use our emoji.
+For more details see the [Emoji One license information](http://emojione.com/developers)
+
+## Licenses
+
+#### Emoji One Artwork
+
+*  Applies to all PNG and SVG files as well as any adaptations made.
+*  License: Creative Commons Attribution-ShareAlike 4.0 International
+*  Human Readable License: http://creativecommons.org/licenses/by-sa/4.0/
+*  Complete Legal Terms: http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+
+#### Emoji One Non-Artwork
+
+*  Applies to the Javascript, JSON, PHP, CSS, HTML files, and everything else not covered under the artwork license above.
+*  License: GPL v2 or later
+*  Complete Legal Terms: http://www.gnu.org/licenses/gpl-2.0.html
---
+++ example/ex01_app.dart
@@ -0,0 +1,37 @@
+library bwu_fontawesome_iconset_svg.example.example01;
+
+import 'dart:html' as dom;
+import 'dart:js' as js;
+import 'package:polymer/polymer.dart';
+
+@CustomTag('ex01-app')
+class Ex03App extends PolymerElement {
+
+  Ex03App.created() : super.created();
+
+  Map iconNames = toObservable({});
+  List categories = toObservable([]); //['emoticons', 'dingbats', 'transport_and_map', 'enclosed_characters', 'uncategorized'];
+  Map prefixes = toObservable({});
+
+  String getPrefix(String category) {
+    return prefixes[category];
+  }
+
+  List<String> getIconNames(String category) {
+    return iconNames[category];
+  }
+
+  attached() {
+    super.attached();
+
+    shadowRoot.querySelectorAll('[is-iconset]').forEach((dom.Element iconset) {
+      String category = iconset.id;
+      categories.add(category);
+      String prefix = iconset.shadowRoot.querySelector('core-iconset-svg').id;
+      prefixes[category] = prefix;
+      List<dom.Element> icons = iconset.shadowRoot.querySelector('core-iconset-svg > svg').querySelectorAll('defs > g[id]');
+      var tmp = icons.map((i) => '${prefix}:${i.id}').toList()..sort();
+      iconNames[category] = tmp;
+    });
+  }
+}
